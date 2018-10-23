0d85c336cae624ff45935ba6e500b34d5d41823e
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-10-23 11:17:35 +0200

    terminar

---
+++ boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,11 @@
+
+package boletin4_2;
+
+public class Boletin4_2 {
+
+    public static void main(String[] args) {
+       Satelite posicionActual= new Satelite(10,50.3,69);
+       posicionActual.verPosicion();
+    }
+    
+}
