e80d23f788502caf94a30dd99b3fd1b38f896571
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-10-24 11:14:02 +0200

    correccion

--- boletin4_4/src/boletin4_4/Boletin4_4.java
+++ boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -6,8 +6,8 @@ public class Boletin4_4 {
         Restaurante obxResultado = new Restaurante();
 
         Restaurante obxResultado2 = new Restaurante(8, 6);
-        obxResultado2.setKgPolbo(8);
-        obxResultado2.setKgPatacas(6);
+//        obxResultado2.setKgPolbo(8); //si declaramos uno no hace falta declarar esto
+//        obxResultado2.setKgPatacas(6);
         obxResultado2.engadirPatacas(3);
         obxResultado2.engadirPolbo(4);
         obxResultado2.amosarPatacas();
