423097000ee35591156aef0172a43b3864150cd4
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-10-24 11:13:03 +0200

    correccion

--- boletin4_4/src/boletin4_4/Restaurante.java
+++ boletin4_4/src/boletin4_4/Restaurante.java
@@ -1,41 +1,48 @@
 package boletin4_4;
 
-
 public class Restaurante {
-   private int kgPolbo;
-   private int kgPatacas;
 
-   public Restaurante(){
-       kgPolbo= 0;
-       kgPatacas= 0;
-   }
-   public Restaurante (int kgPolbo, int kgPatacas){
-       this.kgPolbo= kgPolbo;
-       this.kgPatacas= kgPatacas;
-   }
-   public void setKgPolbo(int kgPolbo){
-       this.kgPolbo= kgPolbo;
-   }
-   public void setKgPatacas(int kgPatacas){
-       this.kgPatacas= kgPatacas;
-   }
+    private int kgPolbo;
+    private int kgPatacas;
 
-   public void engadirPolbo(int x){
-       kgPolbo= kgPolbo+x;
-   }
-   public void engadirPatacas(int x){
-       kgPatacas= kgPatacas+x;
-   }
-      public int amosarNumClientes(){
-       int numClientes;
-       numClientes= (3*kgPolbo*kgPatacas)/2;//regla de tres
-       System.out.println("Se puede atender a "+numClientes+" clientes.");
-       return numClientes;
-   }
-   public void amosarPolbo(){
-       System.out.println("Hay "+kgPolbo+" kg de pulpo en el almacén.");
-   }
-   public void amosarPatacas(){
-       System.out.println("Hay "+kgPatacas+" kg de patatas en el almacén.");
-   }
+    public Restaurante() {
+        kgPolbo = 0;
+        kgPatacas = 0;
+    }
+
+    public Restaurante(int kgPolbo, int kgPatacas) {
+        this.kgPolbo = kgPolbo;
+        this.kgPatacas = kgPatacas;
+    }
+
+    public void setKgPolbo(int kgPolbo) {
+        this.kgPolbo = kgPolbo;
+    }
+
+    public void setKgPatacas(int kgPatacas) {
+        this.kgPatacas = kgPatacas;
+    }
+
+    public void engadirPolbo(int x) {
+        kgPolbo = kgPolbo + x;
+    }
+
+    public void engadirPatacas(int x) {
+        kgPatacas = kgPatacas + x;
+    }
+
+    public int amosarNumClientes() {
+        int numClientes;
+        numClientes = (3 * kgPolbo * kgPatacas) / 2;//regla de tres
+        System.out.println("Se puede atender a " + numClientes + " clientes.");
+        return numClientes;
+    }
+
+    public void amosarPolbo() {
+        System.out.println("Hay " + kgPolbo + " kg de pulpo en el almacén.");
+    }
+
+    public void amosarPatacas() {
+        System.out.println("Hay " + kgPatacas + " kg de patatas en el almacén.");
+    }
 }
